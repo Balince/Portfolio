295547f7a8204a8ba2a9d9a1f3324b15ac34911c
Balince <mbal139@gmail.com>
2021-02-09 15:48:42 -0500

    lots of changes, clean and simple look

--- js/main.js
+++ js/main.js
@@ -1,38 +1,47 @@
 $(document).ready(function () {
 
-let proj1 = $(".projInfo1")
-let proj2 = $(".projInfo2")
-let proj3 = $(".projInfo3")
+    $(".navLink").click(function(){
+        $(".container").addClass("animate-out")
+    });
+
+// let proj1 = $(".projInfo1")
+// let proj2 = $(".projInfo2")
+// let proj3 = $(".projInfo3")
 
 
-// (function($) {
-    $(function() {
-        $(".jEnd").accordion({ 
-            header: "h3", 
-            collapsible: true, 
-            active: 0, 
-            animate: 100,
-            heightStyle: "content"
-        });
-   });
-// })(jQuery);
+// // (function($) {
+//     $(function() {
+//         $(".jEnd").accordion({ 
+//             header: "h3", 
+//             collapsible: true, 
+//             active: 0, 
+//             animate: 100,
+//             heightStyle: "content"
+//         });
+//    });
+// // })(jQuery);
 
 
-$(".button1").on("click", function(){
-    proj1.show();
-    proj2.hide("explode");
-    proj3.hide("explode");
+// $(".button1").on("click", function(){
+//     proj1.show();
+//     proj2.hide("explode");
+//     proj3.hide("explode");
+// });
+
+// $(".button2").on("click", function() {
+//     proj2.show();
+//     proj1.hide("blind", {direction: "right"}, 1000);
+//     proj3.hide("explode");
+// });
+
+// $(".button3").on("click", function() {
+//     proj3.show();
+//     proj1.hide( "explode", { direction: "up" });
+//     proj2.hide( "explode", { direction: "up" });
+// })
+
+
+
+
 });
 
-    $(".button2").on("click", function() {
-    proj2.show();
-    proj1.hide("blind", {direction: "right"}, 1000);
-    proj3.hide("explode");
-});
-
-    $(".button3").on("click", function() {
-    proj3.show();
-    proj1.hide( "explode", { direction: "up" });
-    proj2.hide( "explode", { direction: "up" });
-})
-});
